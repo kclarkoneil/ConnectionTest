9f88d7daeca88706ebd50ccaa36c18c157ff482e
Christopher Clark-O'Neil <kit@apprate.co>
2020-09-03 03:07:19 -0700

    Initial Commit

---
+++ ConnectionTest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ConnectionTest
+//
+//  Created by Kit Clark-O'Neil on 2020-09-03.
+//  Copyright © 2020 Christopher Clark-O'Neil. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ ConnectionTestTests/ConnectionTestTests.swift
@@ -0,0 +1,34 @@
+//
+//  ConnectionTestTests.swift
+//  ConnectionTestTests
+//
+//  Created by Kit Clark-O'Neil on 2020-09-03.
+//  Copyright © 2020 Christopher Clark-O'Neil. All rights reserved.
+//
+
+import XCTest
+@testable import ConnectionTest
+
+class ConnectionTestTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
